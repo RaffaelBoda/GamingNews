b3a9634475e770660ffa920fa1dc6eb2d73656a1
Raffael Boda <raffaelboda1234@gmail.com>
2023-02-21 10:54:18 +0100

    spring init

---
+++ gaming-news/src/main/java/com/codecool/gamingnews/GamingNewsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.gamingnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingNewsApplication.class, args);
+	}
+
+}
